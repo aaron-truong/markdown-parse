2e6b3a9b43d81f7ed8b602ebe4a14712f6873fbe
aaron-truong <47168113+aaron-truong@users.noreply.github.com>
2022-02-02 18:18:27 -0800

    Update MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,9 +14,23 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws IOException {
-        String contents = Files.readString(Path.of("/Users/Aaron/Documents/Github/markdown-parse/breaking-test-file.md"));
-        assertEquals(List.of("https://something.com"), MarkdownParse.getLinks(contents));
+    public void testFile1() throws IOException {
+        String contents=Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
 
+        contents = Files.readString(Path.of("breaking-test-file.md"));
+        expect = List.of("https://something.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+
+        contents = Files.readString(Path.of("./breaking-test-file2.md"));
+        expect = List.of();
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+
+        contents = Files.readString(Path.of("./breaking-test-file3.md"));
+        expect = List.of("something.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
+
+
 }
